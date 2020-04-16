d31bfe70608e9d4e74593355e8849f2319b69574
niascode <niasadmin@niasnebraska.com>
2020-04-16 01:47:59 +0000

    Another guess!

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -14,8 +14,9 @@ public class PalTrackerApplication {
 @Bean
 JdbcTimeEntryRepository getTimeEntryRepository(){
     MysqlDataSource dataSource = new MysqlDataSource();
-    if (System.getenv("DATABASE_URL")!= null){
-        dataSource.setUrl(System.getenv("DATABASE_URL"));
+    final String VAR_NAME = "cloud.services.p.mysql.jdbcUrl";
+    if (System.getenv(VAR_NAME)!= null){
+        dataSource.setUrl(System.getenv(VAR_NAME));
     }else {
         dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
     }
